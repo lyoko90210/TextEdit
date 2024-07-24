1ee5d1d0d9f4480f47548e286ff169644bb71215
lyoko90210 <danieldennis801@gmail.com>
2024-07-24 13:38:44 -0400

    src sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -24,24 +24,20 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+
+
+
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-
-const assetCache = new CacheFirst({
-  cacheName: 'asset-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxEntries: 60, 
-      maxAgeSeconds: 30 * 24 * 60 * 60, 
-    }),
-  ],
-});
-
 registerRoute(
   
-  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'image',
-  assetCache 
-);
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }));
